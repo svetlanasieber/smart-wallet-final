aed50804a8208831414464b33e84b11a79280eef
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-05 21:24:01 +0100

    Update WalletService.java

--- smart-wallet-application/src/main/java/app/wallet/service/WalletService.java
+++ smart-wallet-application/src/main/java/app/wallet/service/WalletService.java
@@ -48,9 +48,6 @@ public class WalletService {
         List<Wallet> allUserWallets = walletRepository.findAllByOwnerUsername(user.getUsername());
         Subscription activeSubscription = user.getSubscriptions().get(0);
 
-        // Pesho
-        // Subs: Premium
-        // Wallets: 2
 
         boolean isDefaultPlanAndMaxWalletsUnlocked = activeSubscription.getType() == SubscriptionType.DEFAULT && allUserWallets.size() == 1;
         boolean isPremiumPlanAndMaxWalletsUnlocked = activeSubscription.getType() == SubscriptionType.PREMIUM && allUserWallets.size() == 2;
@@ -146,11 +143,6 @@ public class WalletService {
                     "Invalid criteria for transfer");
         }
 
-        // Money Transfer
-        // Ivan -> Gosho | 20 EUR
-        // Ivan -20.00 EUR
-        // Gosho +20.00 EUR
-
         Transaction withdrawal = charge(sender, senderWallet.getId(), transferRequest.getAmount(), transferDescription);
         if (withdrawal.getStatus() == TransactionStatus.FAILED) {
             return withdrawal;
@@ -209,7 +201,7 @@ public class WalletService {
 
         walletRepository.save(wallet);
 
-        // Успешно плащане
+     
         System.out.printf("Thread [%s]: Code in WalletService.class\n", Thread.currentThread().getName());
         PaymentNotificationEvent event = PaymentNotificationEvent.builder()
                 .userId(user.getId())
@@ -217,8 +209,7 @@ public class WalletService {
                 .email(user.getEmail())
                 .amount(amount)
                 .build();
-        // Ако искате да публикувате евент, просто разкоментирайте реда по-долу
-        // eventPublisher.publishEvent(event);
+      
 
         return transactionService.createNewTransaction(user,
                 wallet.getId().toString(),
