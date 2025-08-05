be7183ee779938b02aa31a22bb25c78848b2f5e1
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-08-05 15:09:33 +0200

    Update HelloWorldScheduler.java

--- smart-wallet-application/src/main/java/app/scheduler/HelloWorldScheduler.java
+++ smart-wallet-application/src/main/java/app/scheduler/HelloWorldScheduler.java
@@ -8,10 +8,10 @@ import java.time.LocalDateTime;
 @Component
 public class HelloWorldScheduler {
 
-    // Scheduled Job = Cron Job
-//    @Scheduled(fixedDelay = 10000)
+
     public void sayHellowEvery10Seconds() {
 
         System.out.println(LocalDateTime.now() + " Hello World!!!!");
     }
 }
+
