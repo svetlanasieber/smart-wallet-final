72eb4bae4d05f87cc892a51047f5ad80d0b9daf2
Sieberli <135451084+svetlanasieber@users.noreply.github.com>
2025-08-05 15:14:37 +0200

    Update UserController.java

--- smart-wallet-application/src/main/java/app/web/UserController.java
+++ smart-wallet-application/src/main/java/app/web/UserController.java
@@ -28,10 +28,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    // hasAnyRole - проверяваме за една от следните роли
-    // hasRole - проверяваме за една конкретна роля
-    // hasAuthority - проверяваме за един permission
-    // hasAnyAuthority - проверяваме за един от следните permissions
+
     @GetMapping
     @PreAuthorize("hasRole('ADMIN')")
     public ModelAndView getAllUsers(@AuthenticationPrincipal AuthenticationMetadata authenticationMetadata) {
@@ -45,7 +42,7 @@ public class UserController {
         return modelAndView;
     }
 
-    // Endpoint:  '/users/{placeholder}/profile'- unique (just single in my app)
+    
     @GetMapping("/{id}/profile")
     public ModelAndView getProfileMenu(@PathVariable UUID id) {
 
@@ -92,3 +89,4 @@ public class UserController {
         return "redirect:/users";
     }
 }
+
